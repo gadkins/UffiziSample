a6b65ec9a1418b69f4cc8ed76c9bde3656a8b1c5
Adam Sheaffer <adam.e.sheaffer@gmail.com>
2021-01-09 13:54:07 -0600

    add migration

--- SampleApplication/Startup.cs
+++ SampleApplication/Startup.cs
@@ -45,7 +45,7 @@ namespace SampleApplication
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ApplicationDbContext ctx)
         {
             if (env.IsDevelopment())
             {
@@ -53,6 +53,8 @@ namespace SampleApplication
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "SampleApplication v1"));
             }
+
+            ctx.Database.Migrate();
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
