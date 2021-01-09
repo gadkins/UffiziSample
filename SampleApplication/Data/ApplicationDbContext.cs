6b067c379d2c55cfca69b08d87bea5d6ca166d10
Adam Sheaffer <adam.e.sheaffer@gmail.com>
2021-01-09 12:58:45 -0600

    add sample model and dbcontext

---
+++ SampleApplication/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SampleApplication.Models;
+
+namespace SampleApplication.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Joke> Joke { get; set; }
+    }
+}
---
+++ SampleApplication/Models/Joke.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SampleApplication.Models
+{
+    public class Joke
+    {
+        public int Id { get; set; }
+        public string Setup { get; set; }
+        public string Punchline { get; set; }
+    }
+}
